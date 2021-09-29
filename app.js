48123e5799abc87cb580c9bb9c5f2122134d04e3
zulhusni hariz <zulhusnihariz97@gmail.com>
2021-09-29 14:08:56 +0800

    add cookie parser

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const models = require('./models');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
@@ -16,6 +17,8 @@ const app = express();
 // Extended
 const swaggerOptions = {
   swaggerDefinition: {
+    swagger: '2.0',
+    // openapi: '3.0.1',
     info: {
       title: 'Senang Backend',
       description: 'Senang API Documentation',
@@ -24,8 +27,24 @@ const swaggerOptions = {
       },
       servers: ['http://localhost:8000'],
     },
+    // components: {
+    //   securitySchemes: {
+    //     bearerAuth: {
+    //       type: 'http',
+    //       scheme: 'bearer',
+    //       bearerFormat: 'JWT',
+    //     },
+    //   },
+    // },
+    // security: [
+    //   {
+    //     bearerAuth: [],
+    //   },
+    // ],
   },
-  apis: ['app.js'],
+
+  apis: ['./docs/*.js'],
+  // apis: ['app.js'],
 };
 
 // Extended  https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.0.3.md#infoObject
@@ -44,10 +63,26 @@ var authenticationRouter = require('./routes/authentication');
 var productRouter = require('./routes/product');
 var locationRouter = require('./routes/location');
 
+app.use(cookieParser());
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(morgan('combined'));
-app.use(cors());
+
+app.use(
+  cors({
+    origin: `http://localhost:8080`,
+    credentials: true,
+  }),
+);
+
+// app.all('*', function (req, res) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Content-Type,Content-Length, Authorization, Accept,X-Requested-With',
+//   );
+//   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+// });
 
 let verifyUser = async (req, res, next) => {
   console.log(req.headers['x-token']);
@@ -67,6 +102,7 @@ app.use(`${baseURL}/auth`, authenticationRouter);
 
 app.use(`${baseURL}/`, indexRouter);
 app.use(`${baseURL}/user`, userRouter);
+// app.use(`${baseURL}/product`, productRouter);
 app.use(`${baseURL}/product`, verifyUser, productRouter);
 app.use(`${baseURL}/location`, locationRouter);
 
